008873ab0b51e0076a19474dbbd7de702768f826
abdallah abdelmoaty <abdallah.abdelmoaty@tasahel.local>
2024-07-02 17:46:56 +0300

    add filiter

--- Script/app.js
+++ Script/app.js
@@ -1,16 +1,15 @@
-const btn_burger = document.querySelector('.menu-btn__burger');
-const menu = document.querySelector('nav');
-const clsoe_menu = document.querySelector('.menu-btn__close');
+// const btn_burger = document.querySelector('.menu-btn__burger');
+// const menu = document.querySelector('nav');
+// const clsoe_menu = document.querySelector('.menu-btn__close');
 const project_card = document.querySelectorAll('.project_card');
-console.log(menu);
-btn_burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    }
-);
-clsoe_menu.addEventListener('click', () => {
-    menu.classList.remove('active');
-    }
-);
+// btn_burger.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+//     }
+// );
+// clsoe_menu.addEventListener('click', () => {
+//     menu.classList.remove('active');
+//     }
+// );
 function debounce(func, wait = 10, immediate = true) {
     let timeout;
     return function () {
