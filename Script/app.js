0eebd71f5015d135d6a2ead764bc2017c9263c08
abdallah abdelmoaty <abdallah.abdelmoaty@tasahel.local>
2024-07-02 17:12:21 +0300

    add filiter

--- Script/app.js
+++ Script/app.js
@@ -41,15 +41,12 @@ function debounce(func, wait = 10, immediate = true) {
   window.addEventListener('resize', debounce(checkFade));
   document.addEventListener('DOMContentLoaded', checkFade);
 
-  ["website","landing","store","tranning"].forEach(((ele,index)=>{
-    const website = document.getElementById("website");
-    const landing = document.getElementById("landing");
-    const store = document.getElementById("store");
-    const tranning = document.getElementById("tranning");
-    console.log(website,"website");
-    console.log(landing,"landing");
-    console.log(store,"store");
-    console.log(tranning,"tranning");
-  }))
-  
+  const website = document.getElementById("website");
+  const landing = document.getElementById("landing");
+  const store = document.getElementById("store");
+  const tranning = document.getElementById("tranning");
+  console.log(website,"website");
+  console.log(landing,"landing");
+  console.log(store,"store");
+  console.log(tranning,"tranning");
 
